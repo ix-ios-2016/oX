a2deb9351e4bfebb307a7e81437e1a517ea65a0d
Brian Ge <briange1997@gmail.com>
2016-05-30 16:26:52 +0200

    Finished NoughtsAndCrosses all files

--- NoughtsAndCrosses/NoughtsAndCrosses/BoardViewController.swift
+++ NoughtsAndCrosses/NoughtsAndCrosses/BoardViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class BoardViewController: UIViewController {
     
+    var game = OXGame()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,4 +20,42 @@ class BoardViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
+    @IBOutlet var boardView: UIView!
+    @IBOutlet var buttons: [UIButton]!
+    
+    @IBAction func boardTapped(sender: UIButton) {
+        
+        let tag = sender.tag
+        print("board tapped \(tag)")
+        
+        let turn = game.whosTurn()
+        
+        game.playMove(tag)
+        sender.setTitle(String(turn), forState: UIControlState.Normal)
+        
+        let gameState = game.state()
+        if gameState == OXGameState.complete_someone_won {
+            print("Winner: Player " + String(turn))
+            restartGame()
+        }
+        else if gameState == OXGameState.complete_no_one_won {
+            print("Game Tied")
+            restartGame()
+        }
+        
+    }
+    
+    func restartGame() {
+        game.reset()
+        for button in buttons {
+            button.setTitle("", forState: UIControlState.Normal)
+        }
+    }
+    
+    @IBAction func newGame(sender: UIButton) {
+        restartGame()
+    }
+    
+    
+    
 }
--- NoughtsAndCrosses/NoughtsAndCrosses/OXGame.swift
+++ NoughtsAndCrosses/NoughtsAndCrosses/OXGame.swift
@@ -7,3 +7,124 @@
 //
 
 import Foundation
+
+enum CellType : String {
+    case O = "O"
+    case X = "X"
+    case EMPTY = ""
+}
+
+enum OXGameState : String {
+    case inProgress = "In Progress"
+    case complete_no_one_won = "No Winner"
+    case complete_someone_won = "Winner!"
+}
+
+class OXGame {
+    
+    private var board = [CellType](count: 9, repeatedValue: CellType.EMPTY)
+    
+    private func turn() -> Int {
+        
+        var count = 0
+        for cell in board {
+            if cell != CellType.EMPTY {
+                count += 1
+            }
+        }
+        return count
+    }
+    
+    func whosTurn() -> CellType {
+        
+        if (turn() % 2) == 0 {
+            return CellType.X
+        }
+        else {
+            return CellType.O
+        }
+        
+    }
+    
+    func typeAtIndex(index: Int) -> CellType {
+        
+        switch board[index] {
+        case CellType.O:
+            return CellType.O
+        case CellType.X:
+            return CellType.X
+        default:
+            return CellType.EMPTY
+        }
+        
+    }
+    
+    func playMove(index: Int) -> CellType {
+        
+        let cellType = whosTurn()
+        board[index] = cellType
+        return cellType
+        
+    }
+    
+    func winDetection() -> Bool {
+        
+        func check(cell: Int, with: Int, and: Int) -> Bool {
+            return typeAtIndex(cell) != CellType.EMPTY && typeAtIndex(cell) == typeAtIndex(with) && typeAtIndex(with) == typeAtIndex(and)
+        }
+        
+        if check(0, with: 1, and: 2) || check(0, with: 3, and: 6) || check(0, with: 4, and: 8) || check(3, with: 4, and: 5) || check(6, with: 7, and: 8) || check(1, with: 4, and: 7) || check(2, with: 5, and: 8) || check(2, with: 4, and: 6) {
+            return true
+        }
+        else {
+            return false
+        }
+        
+        /*if (typeAtIndex(0) == typeAtIndex(1) && typeAtIndex(1) == typeAtIndex(2) && typeAtIndex(0) != CellType.EMPTY)
+            || (typeAtIndex(0) == typeAtIndex(3) && typeAtIndex(3) == typeAtIndex(6) && typeAtIndex(0) != CellType.EMPTY)
+            || (typeAtIndex(0) == typeAtIndex(4) && typeAtIndex(4) == typeAtIndex(8) && typeAtIndex(0) != CellType.EMPTY)
+            || (typeAtIndex(3) == typeAtIndex(4) && typeAtIndex(4) == typeAtIndex(5) && typeAtIndex(3) != CellType.EMPTY)
+            || (typeAtIndex(6) == typeAtIndex(7) && typeAtIndex(7) == typeAtIndex(8) && typeAtIndex(6) != CellType.EMPTY)
+            || (typeAtIndex(1) == typeAtIndex(4) && typeAtIndex(4) == typeAtIndex(7) && typeAtIndex(1) != CellType.EMPTY)
+            || (typeAtIndex(2) == typeAtIndex(5) && typeAtIndex(5) == typeAtIndex(8) && typeAtIndex(2) != CellType.EMPTY)
+            || (typeAtIndex(2) == typeAtIndex(4) && typeAtIndex(4) == typeAtIndex(6) && typeAtIndex(2) != CellType.EMPTY) {
+            return true
+        }
+        else {
+            return false
+        }*/
+        
+        /*if (board[0] == board[1] && board[1] == board[2] && board[0] != CellType.EMPTY)
+            || (board[0] == board[3] && board[3] == board[6] && board[0] != CellType.EMPTY)
+            || (board[0] == board[4] && board[4] == board[8] && board[0] != CellType.EMPTY)
+            || (board[1] == board[4] && board[4] == board[7] && board[1] != CellType.EMPTY)
+            || (board[2] == board[5] && board[5] == board[8] && board[2] != CellType.EMPTY)
+            || (board[3] == board[4] && board[4] == board[5] && board[3] != CellType.EMPTY)
+            || (board[6] == board[7] && board[7] == board[8] && board[6] != CellType.EMPTY)
+            || (board[2] == board[4] && board[4] == board[6] && board[2] != CellType.EMPTY) {
+            return true
+        }
+        else {
+            return false
+        }*/
+        
+    }
+    
+    func state() -> OXGameState {
+        let won = winDetection()
+        if won {
+            return OXGameState.complete_someone_won
+        }
+        else if turn() == 9 {
+            return OXGameState.complete_no_one_won
+        }
+        else {
+            return OXGameState.inProgress
+        }
+    }
+    
+    func reset() {
+        board = [CellType](count: 9, repeatedValue: CellType.EMPTY)
+    }
+    
+}
