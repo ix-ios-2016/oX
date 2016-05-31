af501101697bc5671740d3e3064b8ebc699f2a7d
Alyssa Porto <app9gv@virginia.edu>
2016-05-31 06:37:58 +0200

    Noughts and Crosses Submission

--- NoughtsAndCrosses/NoughtsAndCrosses/BoardViewController.swift
+++ NoughtsAndCrosses/NoughtsAndCrosses/BoardViewController.swift
@@ -10,12 +10,64 @@ import UIKit
 
 class BoardViewController: UIViewController {
     
+    var game = OXGame()
+    
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var winningMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func boardTapped(sender: AnyObject) {
+        game.playMove(sender.tag)
+        var cellType = ""
+        if (game.whosTurn() == CellType.X) {
+            cellType = "X"
+        } else if (game.whosTurn() == CellType.O) {
+            cellType = "O"
+        }
+        let gameState = game.state()
+        if (gameState == OXGameState.complete_no_one_won) {
+            winningMessage.text = "Tie! No player wins."
+        } else if (gameState == OXGameState.complete_someone_won) {
+            if (game.whosTurn() == CellType.X) {
+                winningMessage.text = "Player X wins"
+            } else if (game.whosTurn() == CellType.O){
+                winningMessage.text = "Player O wins"
+            } else {
+                winningMessage.text = ""
+            }
+        }
+        sender.setTitle(cellType, forState: UIControlState.Normal)
+    }
+    
+    func restartGame() {
+        
+        let buttons = [button0, button1, button2, button3, button4, button5, button6, button7, button8]
+        for button in buttons {
+            button.setTitle("", forState: UIControlState.Normal)
+        }
+        game.reset()
+
+    }
+    
+    @IBAction func newGameTapped(sender: AnyObject) {
+    
+        restartGame()
+        
     }
 
 }
--- NoughtsAndCrosses/NoughtsAndCrosses/OXGame.swift
+++ NoughtsAndCrosses/NoughtsAndCrosses/OXGame.swift
@@ -7,3 +7,102 @@
 //
 
 import Foundation
+
+private let startType = CellType.X
+
+enum CellType:String {
+
+    case O = "O"
+    case X = "X"
+    case EMPTY = ""
+    
+}
+
+enum OXGameState:String {
+    case inProgress = "In Progress"
+    case complete_no_one_won = "Complete, but no one won"
+    case complete_someone_won = "Complete, and a player won"
+}
+
+class OXGame {
+    
+    private var board = [CellType](count: 9, repeatedValue: CellType.EMPTY)
+    
+    private func turn() -> Int {
+        
+        var count = 0
+        
+        for cell in board {
+            if (cell != CellType.EMPTY) {
+                count += 1
+            }
+            
+        }
+        
+        print("turn is \(count)")
+        return count
+    }
+    
+    func whosTurn() -> CellType {
+        
+        if (turn() % 2 == 0) {
+            return CellType.O
+        } else {
+            return CellType.X
+        }
+        
+    }
+    
+    func typeAtIndex(index:Int) -> CellType {
+        
+        return board[index]
+        
+    }
+    
+    func playMove(move:Int) -> CellType {
+        
+        if (whosTurn() == CellType.X) {
+            board[move] = CellType.X
+            return board[move]
+        } else {
+            board[move] = CellType.O
+            return board[move]
+        }
+        
+    }
+    
+    func winDetection() -> Bool {
+        
+        var winner = false
+        let types = [CellType.X, CellType.O]
+        let winningBoards = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0, 4,8], [2,4,6]]
+        for type in types {
+            for winningBoard in winningBoards {
+                if (board[winningBoard[0]] == type && board[winningBoard[1]] == type && board[winningBoard[2]] == type) {
+                    winner = true
+                }
+            }
+        }
+        return winner
+            
+    }
+        
+    func state() -> OXGameState {
+        
+        if (winDetection() == true) {
+            return OXGameState.complete_someone_won
+        } else if (winDetection() == false && turn() == 9) {
+            return OXGameState.complete_no_one_won
+        } else {
+            return OXGameState.inProgress
+        }
+        
+    }
+        
+    func reset() {
+        
+         board = [CellType](count: 9, repeatedValue: CellType.EMPTY)
+        
+    }
+    
+}
